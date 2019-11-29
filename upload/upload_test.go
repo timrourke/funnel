6cb96bccd4c9da9f0cad235330aaaaa349bd1ba1
Tim Rourke <tim@timrourke.com>
2019-11-29 16:33:58 -0600

    Fix data races

--- upload/upload_test.go
+++ upload/upload_test.go
@@ -10,22 +10,19 @@ import (
 )
 
 type stubS3ManagerUploader struct {
-	inputsPassed []*s3manager.UploadInput
-	expectedReturnValues []*s3manager.UploadOutput
-	expectedErrorValues []error
+	inputsPassed 		 chan *s3manager.UploadInput
+	expectedReturnValues chan *s3manager.UploadOutput
+	expectedErrorValues  chan error
 }
 
 func (s *stubS3ManagerUploader) Upload(input *s3manager.UploadInput, options ...func(*s3manager.Uploader)) (*s3manager.UploadOutput, error) {
-	s.inputsPassed = append(s.inputsPassed, input)
-	ret := s.expectedReturnValues[len(s.expectedReturnValues)-1]
-	err := s.expectedErrorValues[len(s.expectedErrorValues)-1]
-	s.expectedReturnValues = s.expectedReturnValues[:len(s.expectedReturnValues)-1]
-	s.expectedErrorValues = s.expectedErrorValues[:len(s.expectedErrorValues)-1]
-	return ret, err
+	s.inputsPassed <- input
+
+	return <- s.expectedReturnValues, <- s.expectedErrorValues
 }
 
 func TestUploadFilesFromPathToBucket(t *testing.T) {
-	Convey("Should upload single file", t, func() {
+	Convey("Should upload single file", t, func(c C) {
 		file, err := ioutil.TempFile("", "somefile")
 		if err != nil {
 			t.Fatal(err)
@@ -33,21 +30,25 @@ func TestUploadFilesFromPathToBucket(t *testing.T) {
 		defer os.Remove(file.Name())
 
 		stub := &stubS3ManagerUploader{
-			inputsPassed:         []*s3manager.UploadInput{},
-			expectedReturnValues: []*s3manager.UploadOutput{nil},
-			expectedErrorValues:  []error{nil},
+			inputsPassed:         make(chan *s3manager.UploadInput),
+			expectedReturnValues: make(chan *s3manager.UploadOutput),
+			expectedErrorValues:  make(chan error),
 		}
 
-		uploader := s3.NewS3Uploader(stub, "unimportant")
+		go func() {
+			inputPassed := <-stub.inputsPassed
+			stub.expectedReturnValues <- nil
+			stub.expectedErrorValues <- nil
+			c.So(*inputPassed.Key, ShouldEqual, file.Name())
+		}()
 
+		uploader := s3.NewS3Uploader(stub, "unimportant")
 		err = UploadFilesFromPathToBucket([]string{file.Name()}, false, uploader)
 
-		So(err, ShouldBeNil)
-		So(len(stub.inputsPassed), ShouldEqual, 1)
-		So(*stub.inputsPassed[0].Key, ShouldEqual, file.Name())
+		c.So(err, ShouldBeNil)
 	})
 
-	Convey("Should upload a directory", t, func() {
+	Convey("Should upload a directory", t, func(c C) {
 		dirname, err := ioutil.TempDir("", "somedir")
 		if err != nil {
 			t.Fatal(err)
@@ -67,25 +68,27 @@ func TestUploadFilesFromPathToBucket(t *testing.T) {
 		}
 
 		stub := &stubS3ManagerUploader{
-			inputsPassed:         []*s3manager.UploadInput{},
-			expectedReturnValues: []*s3manager.UploadOutput{nil, nil},
-			expectedErrorValues:  []error{nil, nil},
+			inputsPassed:         make(chan *s3manager.UploadInput),
+			expectedReturnValues: make(chan *s3manager.UploadOutput),
+			expectedErrorValues:  make(chan error),
 		}
+
+		go func() {
+			var uploadedKeys []string
+			for input := range stub.inputsPassed {
+				uploadedKeys = append(uploadedKeys, *input.Key)
+				stub.expectedReturnValues <- nil
+				stub.expectedErrorValues <- nil
+			}
+
+			c.So(uploadedKeys, ShouldContain, expectedFilePath1)
+			c.So(uploadedKeys, ShouldContain, expectedFilePath2)
+			c.So(len(uploadedKeys), ShouldEqual, 2)
+		}()
 
 		uploader := s3.NewS3Uploader(stub, "unimportant")
-
 		err = UploadFilesFromPathToBucket([]string{dirname}, false, uploader)
-
-		So(err, ShouldBeNil)
-		So(len(stub.inputsPassed), ShouldEqual, 2)
-
-		var uploadedKeys []string
-		for _, input := range stub.inputsPassed {
-			uploadedKeys = append(uploadedKeys, *input.Key)
-		}
-
-		So(uploadedKeys, ShouldContain, expectedFilePath1)
-		So(uploadedKeys, ShouldContain, expectedFilePath2)
+		c.So(err, ShouldBeNil)
 	})
 
 	Convey("Should fail if no file paths provided", t, func() {
@@ -96,7 +99,6 @@ func TestUploadFilesFromPathToBucket(t *testing.T) {
 		}
 
 		uploader := s3.NewS3Uploader(stub, "unimportant")
-
 		err := UploadFilesFromPathToBucket([]string{}, false, uploader)
 
 		So(err, ShouldNotBeNil)
