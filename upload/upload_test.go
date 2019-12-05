c174ecca3e34ecbe5f31796aa806d188a039dc83
Tim Rourke <tim@timrourke.com>
2019-12-05 06:53:10 -0600

    Improve test coverage for common upload operations

--- upload/upload_test.go
+++ upload/upload_test.go
@@ -24,6 +24,30 @@ func (s *stubS3ManagerUploader) Upload(input *s3manager.UploadInput, options ...
 	return <-s.expectedReturnValues, <-s.expectedErrorValues
 }
 
+func TestNewUploader(t *testing.T) {
+	Convey("Should create a new uploader", t, func() {
+		stub := &stubS3ManagerUploader{
+			inputsPassed:         make(chan *s3manager.UploadInput),
+			expectedReturnValues: make(chan *s3manager.UploadOutput),
+			expectedErrorValues:  make(chan error),
+		}
+
+		logger := logrus.New()
+
+		s3Uploader := s3.NewS3Uploader(stub, "unimportant", logger)
+
+		keyTemplate, err := tpl.NewKeyTemplate("{{ filePath }}", logger)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		uploader := NewUploader(false, false, 10, s3Uploader, keyTemplate, logger)
+
+		So(uploader, ShouldNotBeNil)
+		So(uploader, ShouldHaveSameTypeAs, uploader)
+	})
+}
+
 func TestUploadFilesFromPathToBucket(t *testing.T) {
 	Convey("Should upload single file", t, func(c C) {
 		file, err := ioutil.TempFile("", "somefile")
@@ -59,6 +83,49 @@ func TestUploadFilesFromPathToBucket(t *testing.T) {
 		err = uploader.UploadFilesFromPathToBucket([]string{file.Name()})
 
 		c.So(err, ShouldBeNil)
+	})
+
+	Convey("Should upload single file and then delete it afterward", t, func(c C) {
+		file, err := ioutil.TempFile("", "somefile")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		stub := &stubS3ManagerUploader{
+			inputsPassed:         make(chan *s3manager.UploadInput),
+			expectedReturnValues: make(chan *s3manager.UploadOutput),
+			expectedErrorValues:  make(chan error),
+		}
+
+		go func() {
+			inputPassed := <-stub.inputsPassed
+			stub.expectedReturnValues <- nil
+			stub.expectedErrorValues <- nil
+			c.So(*inputPassed.Key, ShouldEqual, file.Name())
+		}()
+
+		logger := logrus.New()
+
+		s3Uploader := s3.NewS3Uploader(stub, "unimportant", logger)
+
+		keyTemplate, err := tpl.NewKeyTemplate("{{ filePath }}", logger)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		uploader := NewUploader(true, false, 10, s3Uploader, keyTemplate, logger)
+
+		err = uploader.UploadFilesFromPathToBucket([]string{file.Name()})
+
+		c.So(err, ShouldBeNil)
+
+		// Verify that file was deleted by expecting an error of type
+		// *os.PathError from an attempt to stat the file
+		_, err = os.Stat(file.Name())
+		err, ok := err.(*os.PathError)
+		if !ok {
+			t.Errorf("expected error to be of type *os.PathError, got: %T: %s", err, err)
+		}
 	})
 
 	Convey("Should upload a directory", t, func(c C) {
@@ -115,6 +182,60 @@ func TestUploadFilesFromPathToBucket(t *testing.T) {
 		c.So(err, ShouldBeNil)
 	})
 
+	Convey("Should upload multiple file paths", t, func(c C) {
+		dirname, err := ioutil.TempDir("", "somedir")
+		if err != nil {
+			t.Fatal(err)
+		}
+		defer os.RemoveAll(dirname)
+
+		expectedFilePath1 := dirname + "/somefile1"
+		err = ioutil.WriteFile(expectedFilePath1, nil, 0644)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		expectedFilePath2 := dirname + "/somefile2"
+		err = ioutil.WriteFile(expectedFilePath2, nil, 0644)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		stub := &stubS3ManagerUploader{
+			inputsPassed:         make(chan *s3manager.UploadInput),
+			expectedReturnValues: make(chan *s3manager.UploadOutput),
+			expectedErrorValues:  make(chan error),
+		}
+
+		go func() {
+			var uploadedKeys []string
+			for input := range stub.inputsPassed {
+				uploadedKeys = append(uploadedKeys, *input.Key)
+				stub.expectedReturnValues <- nil
+				stub.expectedErrorValues <- nil
+			}
+
+			c.So(uploadedKeys, ShouldContain, expectedFilePath1)
+			c.So(uploadedKeys, ShouldContain, expectedFilePath2)
+			c.So(len(uploadedKeys), ShouldEqual, 2)
+		}()
+
+		logger := logrus.New()
+
+		s3Uploader := s3.NewS3Uploader(stub, "unimportant", logger)
+
+		keyTemplate, err := tpl.NewKeyTemplate("{{ filePath }}", logger)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		uploader := NewUploader(false, false, 10, s3Uploader, keyTemplate, logger)
+
+		err = uploader.UploadFilesFromPathToBucket([]string{expectedFilePath1, expectedFilePath2})
+
+		c.So(err, ShouldBeNil)
+	})
+
 	Convey("Should fail if no file paths provided", t, func() {
 		stub := &stubS3ManagerUploader{
 			inputsPassed:         nil,
@@ -137,5 +258,29 @@ func TestUploadFilesFromPathToBucket(t *testing.T) {
 
 		So(err, ShouldNotBeNil)
 		So(err.Error(), ShouldEqual, "must provide at least one path to a file or directory to upload to AWS S3")
+	})
+
+	Convey("Should fail if multiple file paths provided and watching is enabled", t, func() {
+		stub := &stubS3ManagerUploader{
+			inputsPassed:         nil,
+			expectedReturnValues: nil,
+			expectedErrorValues:  nil,
+		}
+
+		logger := logrus.New()
+
+		s3Uploader := s3.NewS3Uploader(stub, "unimportant", logger)
+
+		keyTemplate, err := tpl.NewKeyTemplate("{{ filePath }}", logger)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		uploader := NewUploader(false, true, 10, s3Uploader, keyTemplate, logger)
+
+		err = uploader.UploadFilesFromPathToBucket([]string{"foo", "bar"})
+
+		So(err, ShouldNotBeNil)
+		So(err.Error(), ShouldEqual, "watching multiple paths not supported")
 	})
 }
